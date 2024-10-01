875aec8f982142e0509627981eeac00555fc93cc
Ahmad Mubarak <ahmadmubarak.01@outlook.com>
2024-10-01 22:47:42 +0400

    Stock Market Forecasting Project - Completed.

--- SMFP_app.py
+++ SMFP_app.py
@@ -15,9 +15,10 @@ import statsmodels.api as sm
 
 
 # Title
-app_name = "Stock Market Forecasting App"
+app_name = "Stock Market Forecasting Portfolio"
 st.title(app_name)
-st.subheader('This app is created to forecast the stock market price.')
+# st.subheader('This app is created to forecast the stock market price.')
+st.write('This app is created to forecast the stock market price.')
 # add an image from online resource
 st.image("https://indiaforensic.com/wp-content/uploads/2012/12/Stock-market12.jpg")
 
@@ -85,7 +86,7 @@ st.write(model.summary())
 st.write("-----")
 
 # predict the future values (Forecasting)
-st.write("<p style='color:green; font-size: 50px; font-weight: bold:'>Forecasting the data</p>",unsafe_allow_html=)
+st.write("<p style='color:green; font-size: 50px; font-weight: bold:'>Forecasting the data</p>",unsafe_allow_html=True)
 forecast_period = st.number_input('Select the number of days to forecast', 1, 365, 10)
 # Prediction
 predictions = model.get_prediction(start=len(data), end=len(data)+forecast_period)
@@ -99,8 +100,57 @@ predictions.reset_index(drop=True, inplace=True)
 st.write("## Predictions", predictions)
 st.write("## Actual Data", data)
 
+# lets plot the data
+fig = go.Figure()
+# Add actual data to the plot
+fig.add_trace(go.Scatter(x=data["Date"], y=data[column], mode='lines', name='Actual', line=dict(color='blue')))
+# Add predicted data to the plot
+fig.add_trace(go.Scatter(x=predictions["Date"], y=predictions["predicted_mean"], mode='lines', name='Predicted', line=dict(color='red')))
+# Set the title and axis labels
+fig.update_layout(title='Actual vs Predicted', xaxis_title='Date', yaxis_title='Price', width=1000, height=400)
+# Display the plot
+st.plotly_chart(fig)
 
+# Add Buttons to show or hide separate plots
+show_plots = False
+if st.button('Show Separate Plots'):
+    if not show_plots:
+        st.write(px.line(x=data["Date"], y=data[column], title='Actual', width=1200, height=400, labels={'x':'Date','y':'Price'}).update_traces(line_color='Blue'))
+        st.write(px.line(x=predictions["Date"], y=predictions["predicted_mean"], width=1200, height=400, labels={'x':'Date','y':'Price'}).update_traces(line_color='red', line_dash='dot'))
+        show_plots = True
+    else:
+        show_plots = False
 
+hide_plots = False
+if st.button("Hide Separate Plots"):
+    if not hide_plots:
+        hide_plots = True
+    else:
+        hide_plots = False
 
+st.write("---")
+st.write("###### About the Author:")
 
+st.write("<p style='color:green; font-size: 25px; font-weight: bold:'>Ahmad Mubarak</p>",unsafe_allow_html=True)
+
+# Paste Youtube icon from online source with link
+st.write("##### Connect with me on Social Media")
+# Add links to my social Media
+# urls
+linkedin_url = "https://img.icons8.com/color/48/000000/linkedin.png"
+github_url = "https://img.icons8.com/fluent/48/000000/github.png"
+youtube_url = "https://img.icons8.com/color/48/000000/youtube-play.png"
+twitter_url = "https://img.icons8.com/color/48/000000/twitter.png"
+
+# Redirect Urls
+linkedin_redirect_url = "https://www.linkedin.com/in/ahmad-mubarak-19861a177"
+github_redirect_url = "https://github.com/Ahmad1998-RPA"
+youtube_redirect_url = "http://www.youtube.com/@ahmaddhanesar"
+twitter_redirect_url = "https://x.com/ahmad_mubarak01"
+
+# Add links to the images
+st.markdown(f'<a href="{github_redirect_url}"><img src="{github_url}" width="60" height="60"></a>'
+            f'<a href="{linkedin_redirect_url}"><img src="{linkedin_url}" width="60" height="60"></a>'
+            f'<a href="{youtube_redirect_url}"><img src="{youtube_url}" width="60" height="60"></a>'
+            f'<a href="{twitter_redirect_url}"><img src="{twitter_url}" width="60" height="60"></a>', unsafe_allow_html=True)
 
